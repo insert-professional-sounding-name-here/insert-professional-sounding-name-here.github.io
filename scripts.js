5642118dd17e5065be4868187c781a3af8b0e362
Sam H <sam.hilland1@icloud.com>
2023-11-21 18:44:57 +0000

    Added MM script

--- scripts.js
+++ scripts.js
@@ -4,5 +4,26 @@
 
 //This is the JavaScript file which contains the script which lets the MazeMap work.
 
+var map = new Mazemap.Map({
+    // container id specified in the HTML
+    container: 'map',
 
-//Does NOT contain scripts yet
+    campuses: 368, // 368 is UCLan
+
+    // Position data:
+    // initial position in lngLat format Student centre loc: 
+    // lng: -2.7071393359127285, lat: 53.76267154967661
+    center: {lng: -2.7071393359127285, lat: 53.76267154967661},
+
+    // Data taken from MazeMap's debugging POI inspector:
+    // https://debug.mazemap.com/poi-inspector/?llz=-2.627235544560108,54.04485640674025,8.146123790563951
+    
+    // initial zoom levels:
+    zoom: 18,
+    zLevel: 3
+});
+
+// Add zoom and rotation controls to the map.
+// This lets users see floors within buildings:
+map.addControl(new Mazemap.mapboxgl.NavigationControl());
+
